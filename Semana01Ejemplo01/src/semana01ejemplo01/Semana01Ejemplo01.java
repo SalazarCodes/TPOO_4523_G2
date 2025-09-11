a0360a02bd846c92424512b09ff5ee47700d6d42
glb_lsalazar <glb_lsalazar@niubiz.com.pe>
2025-09-10 23:32:34 -0500

    Primer commit, subida de codigo base

---
+++ Semana01Ejemplo01/src/semana01ejemplo01/Semana01Ejemplo01.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana01ejemplo01;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Semana01Ejemplo01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int sumando1; //tipo de variable nombre de variable;
+        int sumando2 = 10; // inicialización de variable
+        
+        sumando1 = 15;
+        
+        int suma = sumando1 + sumando2;
+        
+        //System.out.println("hola mundo!!"); // comentario de una línea
+        /**/ // comentario de varias líneas
+        System.out.println(suma);
+        
+        System.out.println("La suma es: " + suma);
+        System.out.println(sumando1 + " + " + sumando2 + " es " + suma);
+        
+    }
+    
+}
